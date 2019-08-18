a9e2a420288d7d0082323242c79691079738a4b8
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-18 21:51:42 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,13 @@
 
 def square_array(array)
+<<<<<<< HEAD
   empty_array = []
   array.each do |num|
     empty_array.push(num**2)
     
   end
   return empty_array
+=======
+  array.each{|array| puts "#{array}" }
+>>>>>>> efdab414c7e11bb4114680df7adb6cefb049697a
 end
